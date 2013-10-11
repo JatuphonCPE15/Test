fa37aeab74022212687ad2c818e6329cc2cc671f
JatuphonCPE15 <jatuphon.th@gmail.com>
2013-10-11 14:08:01 +0700

    test update file

---
+++ test01/grails-app/composers/test01/HomeComposer.groovy
@@ -0,0 +1,9 @@
+package test01
+
+
+class HomeComposer extends zk.grails.Composer {
+
+    def afterCompose = { window ->
+        // initialize components here
+    }
+}
---
+++ test01/test/unit/test01/HomeComposerTests.groovy
@@ -0,0 +1,9 @@
+package test01
+
+
+class HomeComposerTests extends zk.grails.Composer {
+
+    def afterCompose = { window ->
+        // initialize components here
+    }
+}
